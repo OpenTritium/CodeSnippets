5d7a58722cab3189713d9212b5d238bc2d3c50ed
OpenTritium <tritium404@hotmail.com>
2023-10-19 09:52:40 +0800

    更改 size 类型

--- data_structure/SeqStack.c
+++ data_structure/SeqStack.c
@@ -6,11 +6,13 @@
 const unsigned MAX_ELEMENT_QUANTITY = 128; // 128 个元素单位
 
 typedef char *Elements;
+
 typedef char ElementType;
+
 typedef struct {
-  Elements data;    // 栈空间开始的内存地址
-  ElementType *top; // 顶端的栈元素指针
-  u_int8_t size;    // 栈元素数量
+	Elements data;    // 栈空间开始的内存地址
+	ElementType *top; // 顶端的栈元素指针
+	unsigned char size;    // 栈元素数量
 } sqStack, *pSqStack;
 
 /**
@@ -19,11 +21,11 @@ typedef struct {
  * @return 栈对象
  */
 sqStack CreateStack(void) {
-  const u_int8_t STACK_SIZE = MAX_ELEMENT_QUANTITY * sizeof(ElementType);
-  const Elements STACK_DATA = (Elements)malloc(STACK_SIZE);
-  assert(STACK_DATA != NULL);
-  sqStack stk = {STACK_DATA, STACK_DATA, 0};
-  return stk;
+	const unsigned char STACK_SIZE = MAX_ELEMENT_QUANTITY * sizeof(ElementType);
+	const Elements STACK_DATA = (Elements)malloc(STACK_SIZE);
+	assert(STACK_DATA != NULL);
+	sqStack stk = {STACK_DATA, STACK_DATA, 0};
+	return stk;
 }
 
 /**
@@ -33,7 +35,9 @@ sqStack CreateStack(void) {
  * @return true 栈为空
  * @return false 栈不为空
  */
-bool IsNull(const pSqStack pStk) { return pStk->size == 0; }
+bool IsNull(const pSqStack pStk) {
+	return pStk->size == 0;
+}
 
 /**
  * @brief 重置一个栈
@@ -41,10 +45,10 @@ bool IsNull(const pSqStack pStk) { return pStk->size == 0; }
  * @param pStk 栈指针
  */
 void ResetStack(pSqStack pStk) {
-  free(pStk->data);
-  pStk->size = 0;
-  pStk->data = NULL;
-  pStk->top = pStk->data;
+	free(pStk->data);
+	pStk->size = 0;
+	pStk->data = NULL;
+	pStk->top = pStk->data;
 }
 
 /**
@@ -56,14 +60,14 @@ void ResetStack(pSqStack pStk) {
  * @return false 入栈失败
  */
 bool Push(pSqStack pStk, const ElementType e) {
-  if (pStk->size == MAX_ELEMENT_QUANTITY) {
-    printf("Stack was full");
-    return false;
-  }
-  *(pStk->top) = e;
-  pStk->size++;
-  pStk->top++;
-  return true;
+	if (pStk->size == MAX_ELEMENT_QUANTITY) {
+		printf("Stack was full");
+		return false;
+	}
+	*(pStk->top) = e;
+	pStk->size++;
+	pStk->top++;
+	return true;
 }
 
 /**
@@ -74,13 +78,13 @@ bool Push(pSqStack pStk, const ElementType e) {
  * @return false
  */
 bool Pop(pSqStack pStk) {
-  if (IsNull(pStk) == true) {
-    printf("Stack was null");
-    return false;
-  }
-  pStk->top--;
-  pStk->size--;
-  return true;
+	if (IsNull(pStk) == true) {
+		printf("Stack was null");
+		return false;
+	}
+	pStk->top--;
+	pStk->size--;
+	return true;
 }
 
 /**
@@ -89,25 +93,27 @@ bool Pop(pSqStack pStk) {
  * @param pStk 栈指针
  * @return ElementType 栈元素
  */
-ElementType GetTop(const pSqStack pStk) { return *(pStk->top - 1); }
+ElementType GetTop(const pSqStack pStk) {
+	return *(pStk->top - 1);
+}
 
 int main(void) {
-  sqStack stk = CreateStack();
-  if (Push(&stk, 'M'))
-    printf("入栈成功\n");
-  else
-    printf("入栈失败\n");
-  printf("栈顶元素：%c\n", GetTop(&stk));
-  printf("元素个数为：%d\n", stk.size);
-  if (Pop(&stk))
-    printf("出栈成功\n");
-  else
-    printf("出栈失败\n");
-  printf("栈状态为%s\n", (IsNull(&stk)) ? "空" : "不为空");
-  if (Push(&stk, 'N'))
-    printf("入栈成功\n");
-  else
-    printf("入栈失败\n");
-  ResetStack(&stk);
-  printf("释放堆内存\n");
+	sqStack stk = CreateStack();
+	if (Push(&stk, 'M'))
+		printf("入栈成功\n");
+	else
+		printf("入栈失败\n");
+	printf("栈顶元素：%c\n", GetTop(&stk));
+	printf("元素个数为：%d\n", stk.size);
+	if (Pop(&stk))
+		printf("出栈成功\n");
+	else
+		printf("出栈失败\n");
+	printf("栈状态为%s\n", (IsNull(&stk)) ? "空" : "不为空");
+	if (Push(&stk, 'N'))
+		printf("入栈成功\n");
+	else
+		printf("入栈失败\n");
+	ResetStack(&stk);
+	printf("释放堆内存\n");
 }
